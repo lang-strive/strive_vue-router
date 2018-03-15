ce1fa7485e37ec1ae370768668a9eded36495227
Strive <31495553+lang-strive@users.noreply.github.com>
2018-03-15 17:09:56 +0800

    Update router.config.js

--- src/router.config.js
+++ src/router.config.js
@@ -2,16 +2,34 @@ import Home from './components/Home.vue'
 import News from './components/News.vue'
 import Msg from './components/msg.vue'
 
-export default {
-    routes:[
-        {path:'/home',component:Home},
-        {path:'/news',component:News,children:[
-            {path:'/news/:id',component:Msg}
-        ]},
-        {path:'*',redirect:'/home'}
-        
-    ]
+
+const routers =[
+    {
+        path:'/home',
+        component:Home
+    },
+    {
+        path:'/news',
+        component:News,
+        children:[
+            {
+                path:'/news/:id',
+                component:Msg
+            }
+        ]
+    },
+    {
+        path:'*',
+        redirect:'/home'
+    }
+]
+
+const RouterConfig = {
+    mode: 'history',
+    base: __dirname,
+    routes: routers
 }
+export default RouterConfig
 
 
 
